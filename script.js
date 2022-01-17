dcf92b7eccf6b04c9a520b293cfdd0ca2416a562
Godstime2020 <77846464+Godstime2020@users.noreply.github.com>
2022-01-17 03:30:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
-const negative = document.querySelector("#neg")
+
 
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
